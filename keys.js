203ba3003f0fe4c3e2d913df49431b26753614d0
Mimi Chang <mimichang94@yahoo.com>
2017-11-27 21:18:32 +0000

    add hw

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: '<O0T8DMXcFgfjJ1H5CZrqm4N8S>',
+    consumer_secret: '<ZeDdEyDmfi12c8inMWuzy2HIsc0JcS9rXo4jDiOnf29NEkXT0E>',
+    access_token_key: '<935251135698522112-2V7CsY1S8pTBWeQ6hDIIkQULgiq274P>',
+    access_token_secret: '<iM9bYYfxgNHhSvtbcizaE90o6E0DpjIjnio79aFPKD5mQ>',
+};
+
+module.exports = twitterKeys;
